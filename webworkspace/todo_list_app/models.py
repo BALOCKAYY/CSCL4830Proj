d1f2a03442effb97cc73ad84f4c3fcc0178218f2
BALOCKAYY <123591627+BALOCKAYY@users.noreply.github.com>
2025-03-19 15:13:00 -0500

    Delete webworkspace directory

--- webworkspace/todo_list_app/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Todo(models.Model):
-    todo = models.CharField(max_length=100, null=True)
-    due = models.DateField(null=True)  # Use DateField for proper date validation
-
